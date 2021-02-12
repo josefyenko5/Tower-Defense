00feba55d2654133f4e2d7aa2a7ac19cd4a75158
josefyenko5 <56546140+josefyenko5@users.noreply.github.com>
2021-02-13 01:21:59 +0800

    Added Enemy Control

---
+++ Tower Defense Game/Assets/Scripts/EnemySettingsSO.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class EnemySettingsSO : ScriptableObject {
+
+    public float speed = 7;
+    public float stoppingDistance = .7f;
+
+}
--- Tower Defense Game/Assets/Scripts/FollowWaypoint.cs
+++ Tower Defense Game/Assets/Scripts/FollowWaypoint.cs
@@ -4,8 +4,7 @@ using YikonUtility;
 public class FollowWaypoint : MonoBehaviour {
 
     [SerializeField] private Rigidbody rb;
-    [SerializeField] private float stoppingDistance = .1f;
-    [SerializeField] private float speed;
+    [SerializeField] private EnemySettingsSO enemySettingsSO;
     private Transform nextWaypoint;
 
     private void Start () {
@@ -13,10 +12,10 @@ public class FollowWaypoint : MonoBehaviour {
     }
 
     private void Update () {
-        Utility.Distance(transform.position, nextWaypoint.position, stoppingDistance, () => {
+        Utility.Distance(transform.position, nextWaypoint.position, enemySettingsSO.stoppingDistance, () => {
             nextWaypoint = Waypoints.GetNextWaypoint();
         }, () => {
-            rb.position = Vector3.MoveTowards(transform.position, nextWaypoint.position, speed * Time.deltaTime);
+            rb.position = Vector3.MoveTowards(transform.position, nextWaypoint.position, enemySettingsSO.speed * Time.deltaTime);
         });
     }
 
